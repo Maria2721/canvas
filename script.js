4bbc1f5c1bb1c07a7247bf8aca4a8e9ad860ada1
Maria2721 <mashaivvas@gmail.com>
2025-01-01 21:22:15 +0300

    feat: add skeleton template

---
+++ script.js
@@ -0,0 +1,8 @@
+const draw = () => {
+	const canvas = document.getElementById("canvas");
+	if (canvas.getContext) {
+		const ctx = canvas.getContext("2d");
+	}
+};
+
+window.addEventListener("load", draw);
